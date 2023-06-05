90dbbab45e61a853dbc1a00fe02492aef7995ac5
Katherine Vail <jabberduckygame@gmail.com>
2023-06-05 11:59:04 -0500

    completed exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,19 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("mushroom", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //Arrange
+            var wordsmith = new WordSmith();
 
+            //Act
+            var actual = wordsmith.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
